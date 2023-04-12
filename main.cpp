c1e948cc54377bb2fa696d9fc41b7e0b87afd2a7
EvgenyGorulya <evgeny.gorulya@gmail.com>
2023-04-12 14:01:45 +0200

    add 12.04.2023

--- main.cpp
+++ main.cpp
@@ -4,12 +4,11 @@ using namespace std;
 int Factorial(int n)
 {
     if(n == 1) return 1;
-     return n * Factorial(n - 1);
+    return n * Factorial(n - 1);
 }
 
 int main()
 {
-
     int n = 5;
     int resultat = 0;
     resultat = Factorial(n);
